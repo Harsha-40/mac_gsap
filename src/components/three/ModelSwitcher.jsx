b42efdac28fd419dee26acd28e52586447243773
Hima Harsha <harshadasari@Himas-MacBook-Air.local>
2025-10-20 15:59:04 +0530

    Implement 3D Product Viewer Section

--- src/components/three/ModelSwitcher.jsx
+++ src/components/three/ModelSwitcher.jsx
@@ -24,10 +24,13 @@ const moveGroup = (group, x) => {
 }
 
 const ModelSwitcher = ( {scale, isMobile} ) => {
+    const SCALE_LARGE_DESKTOP = 0.08;
+    const SCALE_LARGE_MOBILE = 0.05;
+
     const smallMacBookRef = useRef();
     const largeMacBookRef = useRef();
 
-    const showLargeMacbook = scale === 0.08 || scale === 0.05;
+    const showLargeMacbook = scale === SCALE_LARGE_DESKTOP || scale === SCALE_LARGE_MOBILE;
 
     useGSAP(() => {
         if(showLargeMacbook) {
